04f7c94436b80956f1f9919e44de6fd0b595d7ff
zhihan0210 <95704091+zhihan0210@users.noreply.github.com>
2025-04-18 18:08:06 +0200

    Delete model.py

--- model.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-# obtain f(x_ref) with f=Id for 40 keV and f=Unet for other energies
-def model(input_img, i_e, paramImg):
-    output_img = paramImg.model[i_e](input_img)
-    return output_img
